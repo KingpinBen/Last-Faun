c49c09fd298f826114de71c2f4c8986db1c5867f
BenGriffiths <scare_tfc@hotmail.com>
2013-08-08 16:58:45 +0100

    Fixed bad exiting non-colliding objects
    objects that collided but weren't added were able to trigger
    OnTriggerExit in some situations

--- New Unity Project 1/Assets/scripts/objects/Button/PressurePadScript.cs
+++ New Unity Project 1/Assets/scripts/objects/Button/PressurePadScript.cs
@@ -17,168 +17,186 @@ public class PressurePadScript : InteractiveObject
     private bool _mouseOvered;
     private bool _moving;
     private GameObject _instantiatedParticles;
-    private readonly List<Collider> _collidingObjects = new List<Collider>();
+    private readonly List< Collider > _collidingObjects = new List< Collider >();
     private AINode _oldTarget;
 
-    void Start()
+    protected override void Start()
     {
-        foreach (var t in targetObjects)
+        foreach ( var t in targetObjects )
         {
             OnActivate += t.ToggleObject;
             OnDeactivate += t.ToggleObject;
         }
     }
 
-    void Update()
+    private void Update()
     {
-        if (aiTargetNode.objectActive)
+        if ( aiTargetNode.objectActive )
         {
-            if (_companionWaiting == false && 
-                aiTargetNode.GetAffectingCharacter().waitingAtTarget)
-                    _companionWaiting = true;
+            if ( _companionWaiting == false &&
+                 aiTargetNode.GetAffectingCharacter().waitingAtTarget )
+                _companionWaiting = true;
 
             //  Clicked again, so cancel waiting
-            if (Input.GetMouseButtonDown(0))
+            if ( Input.GetMouseButtonDown( 0 ) )
             {
-                _oldTarget.ActivateObject(this);
-                CursorManager.instance.ChangeCursorStatus(CursorManager.CursorGestureStatus.Normal);
+                _oldTarget.ActivateObject( this );
+                CursorManager.instance.ChangeCursorStatus( CursorManager.CursorGestureStatus.Normal );
                 _companionWaiting = false;
             }
         }
         else
         {
-            if (_mouseOvered)
+            if ( _mouseOvered )
             {
                 var action = aiTargetNode.GetAffectingCharacter().currentAction;
 
-                if (Input.GetMouseButtonDown(0) && 
-                    (action == Character.CharacterAction.None || 
-                    action == Character.CharacterAction.Waiting))
+                if ( Input.GetMouseButtonDown( 0 ) &&
+                     ( action == Character.CharacterAction.None ||
+                       action == Character.CharacterAction.Waiting ) )
                 {
                     _oldTarget = aiTargetNode.GetAffectingCharacter().Target;
 
-                    aiTargetNode.ActivateObject(this);
-                    CursorManager.instance.ChangeCursor(CursorType.Wait);
-                    CursorManager.instance.ChangeCursorStatus(CursorManager.CursorGestureStatus.Capturing);
+                    aiTargetNode.ActivateObject( this );
+                    CursorManager.instance.ChangeCursor( CursorType.Wait );
+                    CursorManager.instance.ChangeCursorStatus( CursorManager.CursorGestureStatus.Capturing );
                 }
             }
         }
 
-        if (_moving)
+        if ( _moving )
         {
-            if (_collidingObjects.Count > 0)
+            if ( _collidingObjects.Count > 0 )
             {
-                padObject.transform.localPosition = Vector3.Slerp(padObject.transform.localPosition, new Vector3(0, -heightDepression, 0),
-                                                        Time.deltaTime * 4.0f);
-                if (Math.Abs(padObject.transform.localPosition.y - -heightDepression) < Mathf.Epsilon)
+                padObject.transform.localPosition = Vector3.Slerp( padObject.transform.localPosition,
+                                                                   new Vector3( 0, -heightDepression, 0 ),
+                                                                   Time.deltaTime * 4.0f );
+                if ( Math.Abs( padObject.transform.localPosition.y - -heightDepression ) < Mathf.Epsilon )
                     _moving = false;
             }
             else
             {
-                padObject.transform.localPosition = Vector3.Slerp(padObject.transform.localPosition, Vector3.zero, Time.deltaTime * 4.0f);
-                if (Math.Abs(padObject.transform.localPosition.y - 0) < Mathf.Epsilon)
+                padObject.transform.localPosition = Vector3.Slerp( padObject.transform.localPosition, Vector3.zero,
+                                                                   Time.deltaTime * 4.0f );
+                if ( Math.Abs( padObject.transform.localPosition.y - 0 ) < Mathf.Epsilon )
                     _moving = false;
             }
         }
     }
 
-    void OnTriggerEnter(Collider body)
+    private void OnTriggerEnter( Collider body )
     {
-        if (body.isTrigger) return;
+        if ( body.isTrigger )
+            return;
 
-        if (requiredObjects.Length > 0)
+        if ( requiredObjects.Length > 0 )
         {
-            for (var i = 0; i < requiredObjects.Length; i++)
+            for ( var i = 0; i < requiredObjects.Length; i++ )
             {
                 var t = requiredObjects[i];
-                if (body.transform != t) continue;
+                if ( body.transform != t )
+                    continue;
 
-                _collidingObjects.Add(body);
+                _collidingObjects.Add( body );
             }
         }
         else
-            _collidingObjects.Add(body);
-
-        if (_collidingObjects.Count == 1) 
-            ToggleObject(this);
+		{
+            _collidingObjects.Add( body );
+		}
+			
+        if ( _collidingObjects.Count == 1 )
+            ToggleObject( this );
     }
-    void OnTriggerExit(Collider body)
+
+    private void OnTriggerExit( Collider body )
     {
-        if (body.isTrigger) return;
-        for (var i = 0; i < _collidingObjects.Count; i++)
+        if ( body.isTrigger )
+            return;
+
+        for ( var i = 0; i < _collidingObjects.Count; i++ )
         {
-            if (_collidingObjects[i] != body) continue;
-            _collidingObjects.RemoveAt(i);
+            if ( _collidingObjects[i] != body )
+                continue;
+			
+            _collidingObjects.RemoveAt( i );
+			
+			if ( _collidingObjects.Count == 0 && holdSwitch )
+            	ToggleObject( this );
+			
             break;
         }
-
-        if (_collidingObjects.Count == 0 && holdSwitch)
-            ToggleObject(this);
     }
 
-    void OnMouseEnter()
+    private void OnMouseEnter()
     {
         //  These objects aren't gesture objects so need to do the gesture mouseover 
         _mouseOvered = true;
-        CursorManager.instance.ChangeCursor(CursorType.Wait);
+        CursorManager.instance.ChangeCursor( CursorType.Wait );
     }
-    void OnMouseExit()
+
+    private void OnMouseExit()
     {
         //  These objects aren't gesture objects so need to do the gesture mouseover 
         _mouseOvered = false;
-        CursorManager.instance.ChangeCursor(CursorType.Normal);
+        CursorManager.instance.ChangeCursor( CursorType.Normal );
     }
 
-    public override void ActivateObject(object sender)
+    public override void ActivateObject( object sender )
     {
-        if (objectActive) return;
+        if ( objectActive )
+            return;
 
-        ToggleObject(sender);
+        ToggleObject( sender );
         _moving = true;
         CreateParticlePrefab();
     }
-    public override void DeactivateObject(object sender)
-    {
-        if (!objectActive) return;
 
-        if (holdSwitch)
-            ToggleObject(sender);
+    public override void DeactivateObject( object sender )
+    {
+        if ( !objectActive )
+            return;
+
+        if ( holdSwitch )
+            ToggleObject( sender );
         else
             objectActive = false;
     }
 
-    public override void ToggleObject(object sender)
+    public override void ToggleObject( object sender )
     {
-        if (objectActive)
-            base.DeactivateObject(sender);
+        if ( objectActive )
+            base.DeactivateObject( sender );
         else
-            base.ActivateObject(sender);
+            base.ActivateObject( sender );
 
         _moving = true;
     }
 
-    void CreateParticlePrefab()
+    private void CreateParticlePrefab()
     {
-        if (!particlePrefab) return;
+        if ( !particlePrefab )
+            return;
 
-        _instantiatedParticles = Instantiate(particlePrefab) as GameObject;
+        _instantiatedParticles = Instantiate( particlePrefab ) as GameObject;
         _instantiatedParticles.transform.parent = transform;
         _instantiatedParticles.transform.localPosition = Vector3.zero;
-        _instantiatedParticles.transform.localEulerAngles = new Vector3(270, 0, 0);
+        _instantiatedParticles.transform.localEulerAngles = new Vector3( 270, 0, 0 );
 
-        Invoke("KillParticlePrefab", 2);
+        Invoke( "KillParticlePrefab", 2 );
     }
-    void KillParticlePrefab()
+
+    private void KillParticlePrefab()
     {
-        Destroy(_instantiatedParticles);
+        Destroy( _instantiatedParticles );
     }
 
-    void OnDrawGizmosSelected()
+    private void OnDrawGizmosSelected()
     {
         Gizmos.color = objectActive ? Color.green : Color.blue;
-        foreach (InteractiveObject t in targetObjects)
+        foreach ( InteractiveObject t in targetObjects )
         {
-            Gizmos.DrawLine(transform.position, t.transform.position);
+            Gizmos.DrawLine( transform.position, t.transform.position );
         }
     }
 }
