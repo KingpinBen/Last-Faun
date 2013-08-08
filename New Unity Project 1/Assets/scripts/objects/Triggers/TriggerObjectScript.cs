a40c9f370ff595519887bf50c432f8dac552d57a
BenGriffiths <scare_tfc@hotmail.com>
2013-08-08 16:46:34 +0100

    Draws line to target if it's a camera

--- New Unity Project 1/Assets/scripts/objects/Triggers/TriggerObjectScript.cs
+++ New Unity Project 1/Assets/scripts/objects/Triggers/TriggerObjectScript.cs
@@ -19,4 +19,21 @@ public class TriggerObjectScript : InteractiveObject
 
         Gizmos.DrawCube(Vector3.zero, transform.localScale);
     }
+
+    void OnDrawGizmosSelected()
+    {
+        if (targetObject)
+        {
+            var isCamera = targetObject as CameraBaseController;
+
+            if (isCamera)
+            {
+                if (isCamera.cameraTransformNode)
+                    Gizmos.DrawLine(transform.position, isCamera.cameraTransformNode.transform.position);
+            }
+            else
+                Gizmos.DrawLine(transform.position, targetObject.transform.position);
+        }
+            
+    }
 }
