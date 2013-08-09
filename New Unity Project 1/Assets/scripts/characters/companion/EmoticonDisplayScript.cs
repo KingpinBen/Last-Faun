6497829b5ad21d2a8c572ae0da43740e9164fa3c
BenGriffiths <scare_tfc@hotmail.com>
2013-08-09 21:20:21 +0100

    Can now find out what emotion state faun is in

--- New Unity Project 1/Assets/scripts/characters/companion/EmoticonDisplayScript.cs
+++ New Unity Project 1/Assets/scripts/characters/companion/EmoticonDisplayScript.cs
@@ -12,13 +12,26 @@ public class EmoticonDisplayScript : MonoBehaviour
 
     public GameObject emoticonObject;
     public Transform head;
+    public int currentEmoticon
+    {
+        get
+        {
+            return _currentEmoticon;
+        }
+        set
+        {
+            _currentEmoticon = value;
+            _animator.SetInteger(_hashes.emotionState, value);
+        }
+    }
 
     private EmoticonScript _emoticon;
     private Animator _animator;
     private CompanionScript _companionScript;
     private CharacterAnimationHashes _hashes;
+    private int _currentEmoticon;
 
-    private int[][] _emoticonStates = new[]
+    private readonly int[][] _emoticonStates = new[]
                                           {
                                               new[]
                                                   {
@@ -36,11 +49,15 @@ public class EmoticonDisplayScript : MonoBehaviour
         _companionScript = GetComponent< CompanionScript >();
         _hashes = GetComponent< CharacterAnimationHashes >();
 
-        _animator.SetLayerWeight( 1, 1 );
-
         var obj = Instantiate( emoticonObject, head.position + Vector3.up, Quaternion.identity ) as GameObject;
         _emoticon = obj.GetComponent< EmoticonScript >();
         _emoticon.transform.parent = transform;
+    }
+
+    void Start()
+    {
+        _animator.SetLayerWeight(1, 1);
+        currentEmoticon = 0;
     }
 
     public void InstantiateEmoticon( Emotions emoticon )
@@ -55,9 +72,9 @@ public class EmoticonDisplayScript : MonoBehaviour
         if ( _companionScript.currentAction == Character.CharacterAction.None ||
              _companionScript.currentAction == Character.CharacterAction.Waiting )
         {
-            _animator.SetInteger( _hashes.emotionState,
-                                  _emoticonStates[( int ) emoticon][
-                                      Random.Range( 0, _emoticonStates[( int ) emoticon].Length )] );
+            currentEmoticon = _emoticonStates
+                [( int ) emoticon]
+                [Random.Range( 0, _emoticonStates[( int ) emoticon].Length )];
         }
 
     }
@@ -66,6 +83,6 @@ public class EmoticonDisplayScript : MonoBehaviour
     {
         yield return new WaitForSeconds( 0.5f );
 
-        _animator.SetInteger( _hashes.emotionState, 0 );
+        currentEmoticon = 0;
     }
 }
