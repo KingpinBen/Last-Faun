c9b33f68648c67ed0b8d9d5d707c4e8c4509bbff
BenGriffiths <scare_tfc@hotmail.com>
2013-08-09 01:29:47 +0100

    Changed to use hashes

--- New Unity Project 1/Assets/scripts/characters/companion/EmoticonDisplayScript.cs
+++ New Unity Project 1/Assets/scripts/characters/companion/EmoticonDisplayScript.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public enum Emotions {
     None = -1, 
-    Good, Bad
+    Bad,
+    Good
 }
 
-public class EmoticonDisplayScript : MonoBehaviour {
+public class EmoticonDisplayScript : MonoBehaviour
+{
 
     public GameObject emoticonObject;
     public Transform head;
@@ -14,49 +16,56 @@ public class EmoticonDisplayScript : MonoBehaviour {
     private EmoticonScript _emoticon;
     private Animator _animator;
     private CompanionScript _companionScript;
+    private CharacterAnimationHashes _hashes;
 
     private int[][] _emoticonStates = new[]
                                           {
-                                              new[] {2, 9, 10, 11, 12, 13, 14, 15, 16},
-                                              new[] {1, 3, 4, 5, 6, 7, 8}
+                                              new[]
+                                                  {
+                                                      2, 9, 10, 11, 12, 13, 14, 15, 16
+                                                  },
+                                              new[]
+                                                  {
+                                                      1, 3, 4, 5, 6, 7, 8
+                                                  }
                                           };
 
-    void Awake() {
-        _animator = GetComponent<Animator>();
-        _companionScript = GetComponent<CompanionScript>();
+    private void Awake()
+    {
+        _animator = GetComponent< Animator >();
+        _companionScript = GetComponent< CompanionScript >();
+        _hashes = GetComponent< CharacterAnimationHashes >();
 
-        if (_animator.layerCount >= 2)
-            _animator.SetLayerWeight(1, 1);
+        _animator.SetLayerWeight( 1, 1 );
 
-        var obj = Instantiate(emoticonObject, head.position + Vector3.up * 2, Quaternion.identity) as GameObject;
-        _emoticon = obj.GetComponent<EmoticonScript>();
+        var obj = Instantiate( emoticonObject, head.position + Vector3.up, Quaternion.identity ) as GameObject;
+        _emoticon = obj.GetComponent< EmoticonScript >();
         _emoticon.transform.parent = transform;
     }
 
-    public void InstantiateEmoticon(Emotions emoticon) {
-        if (emoticon < 0)
-            return;
-
-        _emoticon.SetTexture((int)emoticon);
+    public void InstantiateEmoticon( Emotions emoticon )
+    {
+        _emoticon.SetTexture( ( int ) emoticon );
 
         StopAllCoroutines();
-        StartCoroutine(ResetCurrentEmotionState());
+        StartCoroutine( ResetCurrentEmotionState() );
 
         //  We'll check if he's currently already doing something with his hands
         //  so, for instance, he doesn't cheer whilst holding a boulder
-        if (_companionScript.currentAction == Character.CharacterAction.None ||
-            _companionScript.currentAction == Character.CharacterAction.Waiting)
+        if ( _companionScript.currentAction == Character.CharacterAction.None ||
+             _companionScript.currentAction == Character.CharacterAction.Waiting )
         {
-            _animator.SetInteger("EmotionalState", 
-                _emoticonStates[(int)emoticon][Random.Range(0,_emoticonStates[(int)emoticon].Length)]);
+            _animator.SetInteger( _hashes.emotionState,
+                                  _emoticonStates[( int ) emoticon][
+                                      Random.Range( 0, _emoticonStates[( int ) emoticon].Length )] );
         }
-            
+
     }
 
-    IEnumerator ResetCurrentEmotionState()
+    private IEnumerator ResetCurrentEmotionState()
     {
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds( 0.5f );
 
-        _animator.SetInteger("EmotionalState", 0);
+        _animator.SetInteger( _hashes.emotionState, 0 );
     }
 }
