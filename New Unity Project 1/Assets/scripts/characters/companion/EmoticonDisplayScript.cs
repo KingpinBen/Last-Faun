0b1f540e33ebfc4e8f2a7fe05b4c2b84339880a9
BenGriffiths <scare_tfc@hotmail.com>
2013-08-09 17:51:09 +0100

    Needed to switch array around due to art

--- New Unity Project 1/Assets/scripts/characters/companion/EmoticonDisplayScript.cs
+++ New Unity Project 1/Assets/scripts/characters/companion/EmoticonDisplayScript.cs
@@ -22,11 +22,11 @@ public class EmoticonDisplayScript : MonoBehaviour
                                           {
                                               new[]
                                                   {
-                                                      2, 9, 10, 11, 12, 13, 14, 15, 16
+                                                      1, 3, 4, 5, 6, 7, 8
                                                   },
                                               new[]
                                                   {
-                                                      1, 3, 4, 5, 6, 7, 8
+                                                      2, 9, 10, 11, 12, 13, 14, 15, 16
                                                   }
                                           };
 
