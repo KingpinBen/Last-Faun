27388bb2f5c3ff19d7c521a7a1a9a768eef95bbb
BenGriffiths <scare_tfc@hotmail.com>
2013-08-11 03:29:02 +0100

    Switched to coroutines

--- New Unity Project 1/Assets/scripts/objects/Logic/TimerLogicBehaviourScript.cs
+++ New Unity Project 1/Assets/scripts/objects/Logic/TimerLogicBehaviourScript.cs
@@ -1,39 +1,42 @@
 using UnityEngine;
+using System.Collections;
 
 public class TimerLogicBehaviourScript : InteractiveObject
 {
     public float countdownToActivate = 3.0f; //  Seconds obv.
     public InteractiveObject[] targetObjects;
 
-    private float _timeRemaining;
-
-    private void Start()
+    protected override void Start()
     {
-        _timeRemaining = countdownToActivate;
-    }
-
-    private void Update()
-    {
-        if (!objectActive) return;
-        _timeRemaining -= Time.deltaTime;
-
-        if (_timeRemaining <= 0.0f)
-            TriggerTargetObjects();
+        if (objectActive)
+            ActivateObject(null);
     }
 
     public override void ActivateObject(object sender)
     {
-        if (objectActive) return;
-        //  Reset the time on activate rather than only 
-        //  on deactivate incase we want to just trigger it again.
-        _timeRemaining = countdownToActivate;
+        if (objectActive) 
+            StopAllCoroutines();
+
         base.ActivateObject(sender);
+        StartCoroutine( Countdown() );
+    }
+
+    public override void DeactivateObject(object sender)
+    {
+        base.DeactivateObject(sender);
+        StopAllCoroutines();
+    }
+
+    IEnumerator Countdown()
+    {
+        yield return new WaitForSeconds( countdownToActivate );
+        TriggerTargetObjects();
     }
 
     private void TriggerTargetObjects()
     {
-        foreach (var t in targetObjects)
-            t.ToggleObject(null);
+        for ( int i = 0; i < targetObjects.Length; i++ )
+            targetObjects[i].ToggleObject( this );
 
         DeactivateObject(null);
     }
@@ -41,11 +44,13 @@ public class TimerLogicBehaviourScript : InteractiveObject
     private void OnDrawGizmosSelected()
     {
         if (targetObjects == null) return;
-        foreach (InteractiveObject t in targetObjects)
+        for ( int i = 0; i < targetObjects.Length; i++ )
         {
-            if (!t.gameObject) continue;
+            InteractiveObject t = targetObjects[i];
+            if ( !t )
+                continue;
             Gizmos.color = Color.blue;
-            Gizmos.DrawLine(transform.position, t.transform.position);
+            Gizmos.DrawLine( transform.position, t.transform.position );
         }
     }
 
